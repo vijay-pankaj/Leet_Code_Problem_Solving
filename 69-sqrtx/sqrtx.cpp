e1c2e5e3cd3c11de032624a3eb18f05045a8a7bc
vij@y_pankaJ <138501263+vijay-pankaj@users.noreply.github.com>
2025-05-15 14:26:46 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (50.95%) - LeetSync

--- 69-sqrtx/sqrtx.cpp
+++ 69-sqrtx/sqrtx.cpp
@@ -1,13 +1,6 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        if(x==0){
-            return 0;
-        }
-        int i=1;
-        while((long long)i*i<=x){
-            i++;
-        }
-        return i-1;
+      return sqrt(x);
     }
 };
