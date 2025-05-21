642c34ba0b82dfa8ead6ac09fea8105af19e7330
vij@y_pankaJ <138501263+vijay-pankaj@users.noreply.github.com>
2025-05-21 10:42:59 +0530

    Time: 1 ms (60.21%) | Memory: 8.5 MB (69.13%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -4,7 +4,7 @@ public:
         if(x<0)return false;
         int originalNum=x;
         long rev=0;
-        while(x){
+        while(x>0){
             int rem=x%10;
             rev=rev*10+rem;
             x=x/10;
