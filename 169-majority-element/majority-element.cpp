87bd13ed86b1437cbead4d1ccda1a684102ace08
vij@y_pankaJ <138501263+vijay-pankaj@users.noreply.github.com>
2025-06-14 19:11:36 +0530

    Time: 0 ms (100.00%) | Memory: 28.2 MB (23.83%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -4,9 +4,8 @@ public:
         int count=0;
         int candidate=0;
         for(int num:nums){
-            if(count==0){
-                candidate=num;
-            }if(num==candidate)count++;
+            if(count==0)candidate=num;
+            if(num==candidate)count++;
             else count--;
         }
         return candidate;
