2b90c76d2d6a3b674b10a810684b049f63ad3298
vij@y_pankaJ <138501263+vijay-pankaj@users.noreply.github.com>
2025-06-20 21:51:24 +0530

    Time: 2436 ms (5.03%) | Memory: 7.8 MB (16.80%) - LeetSync

--- 367-valid-perfect-square/valid-perfect-square.cpp
+++ 367-valid-perfect-square/valid-perfect-square.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     bool isPerfectSquare(int num) {
+        if(num<=0)return false;
         for(long i=1;i<=num; i++)if(i*i==num)return true;
         return false;
     }
