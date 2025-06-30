61e99edcee0a2b8396f70c4db05ad3f967eaa711
vij@y_pankaJ <138501263+vijay-pankaj@users.noreply.github.com>
2025-06-30 21:14:54 +0530

    Time: 13 ms (49.84%) | Memory: 24.1 MB (96.95%) - LeetSync

--- 2502-sort-the-people/sort-the-people.cpp
+++ 2502-sort-the-people/sort-the-people.cpp
@@ -5,7 +5,9 @@ public:
         for(int i = 0; i < n - 1; i++) {
             int maxIdx = i;
             for(int j = i + 1; j < n; j++) {
-                if(heights[j] > heights[maxIdx])  maxIdx = j;
+                if(heights[j] > heights[maxIdx]) {
+                    maxIdx = j;
+                }
             }
             swap(heights[i], heights[maxIdx]);
             swap(names[i], names[maxIdx]);
