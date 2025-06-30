cfb24566ef63696af1a3368633edf0557de7e887
vij@y_pankaJ <138501263+vijay-pankaj@users.noreply.github.com>
2025-06-30 21:13:43 +0530

    Time: 16 ms (36.14%) | Memory: 24.2 MB (91.78%) - LeetSync

---
+++ 2502-sort-the-people/sort-the-people.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
+        int n = heights.size();
+        for(int i = 0; i < n - 1; i++) {
+            int maxIdx = i;
+            for(int j = i + 1; j < n; j++) {
+                if(heights[j] > heights[maxIdx]) {
+                    maxIdx = j;
+                }
+            }
+            swap(heights[i], heights[maxIdx]);
+            swap(names[i], names[maxIdx]);
+        }
+        return names;
+    }
+};
