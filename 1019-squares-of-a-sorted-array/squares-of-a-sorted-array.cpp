1530f6817cc6c949c241f28bd584e3256e51a206
vij@y_pankaJ <138501263+vijay-pankaj@users.noreply.github.com>
2025-07-01 18:59:38 +0530

    Time: 905 ms (5.01%) | Memory: 30.3 MB (37.82%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
@@ -6,7 +6,9 @@ public:
         }
         for(int i=0; i<nums.size(); i++){
             for(int j=1; j<nums.size()-i; j++){
-                if(nums[j-1]>nums[j])swap(nums[j-1],nums[j]);
+                if(nums[j-1]>nums[j]){
+                    swap(nums[j-1],nums[j]);
+                }
             }
         }
         return nums;
