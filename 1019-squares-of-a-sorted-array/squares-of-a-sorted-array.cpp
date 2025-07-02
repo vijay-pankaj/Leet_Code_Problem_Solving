265c9fac9da2f93ff17167596581cf21693aeb26
vij@y_pankaJ <138501263+vijay-pankaj@users.noreply.github.com>
2025-07-02 23:19:39 +0530

    Time: 11 ms (19.12%) | Memory: 30.2 MB (85.41%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
@@ -1,56 +1,11 @@
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
-        for (int i = 0; i < nums.size(); i++) {
-            nums[i] = nums[i] * nums[i];
+        for(int i=0; i<nums.size(); i++){
+            nums[i]=nums[i]*nums[i];
         }
-        mergeSort(nums, 0, nums.size() - 1);
-
+        sort(nums.begin(), nums.end());
         return nums;
-    }
 
-    void merge(vector<int>& arr, int left, int mid, int right) {
-        int n1 = mid - left + 1;
-        int n2 = right - mid;
-
-        vector<int> L(n1);
-        vector<int> R(n2);
-
-        for (int i = 0; i < n1; i++)
-            L[i] = arr[left + i];
-        for (int j = 0; j < n2; j++)
-            R[j] = arr[mid + 1 + j];
-
-        int i = 0, j = 0, k = left;
-
-        while (i < n1 && j < n2) {
-            if (L[i] <= R[j]) {
-                arr[k] = L[i];
-                i++;
-            } else {
-                arr[k] = R[j];
-                j++;
-            }
-            k++;
-        }
-        while (i < n1) {
-            arr[k] = L[i];
-            i++;
-            k++;
-        }
-        while (j < n2) {
-            arr[k] = R[j];
-            j++;
-            k++;
-        }
-    }
-
-    void mergeSort(vector<int>& arr, int left, int right) {
-        if (left < right) {
-            int mid = left + (right - left) / 2;
-            mergeSort(arr, left, mid);
-            mergeSort(arr, mid + 1, right);
-            merge(arr, left, mid, right);
-        }
     }
 };
