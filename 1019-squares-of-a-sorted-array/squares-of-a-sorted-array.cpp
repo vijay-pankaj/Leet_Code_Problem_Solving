d15af6437cdbc4f5196bc8cf15738e69b8135f36
vij@y_pankaJ <138501263+vijay-pankaj@users.noreply.github.com>
2025-07-02 23:17:13 +0530

    Time: 79 ms (5.04%) | Memory: 58 MB (6.11%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
@@ -4,30 +4,53 @@ public:
         for (int i = 0; i < nums.size(); i++) {
             nums[i] = nums[i] * nums[i];
         }
-        quickSort(nums, 0, nums.size() - 1);
+        mergeSort(nums, 0, nums.size() - 1);
 
         return nums;
     }
 
-    void quickSort(vector<int>& arr, int low, int high) {
-        if (low < high) {
-            int pi = part(arr, low, high);
-            quickSort(arr, low, pi - 1);
-            quickSort(arr, pi + 1, high);
+    void merge(vector<int>& arr, int left, int mid, int right) {
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+
+        vector<int> L(n1);
+        vector<int> R(n2);
+
+        for (int i = 0; i < n1; i++)
+            L[i] = arr[left + i];
+        for (int j = 0; j < n2; j++)
+            R[j] = arr[mid + 1 + j];
+
+        int i = 0, j = 0, k = left;
+
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
         }
     }
 
-    int part(vector<int>& arr, int low, int high) {
-        int pivot = arr[high];
-        int i = low - 1;
-
-        for (int j = low; j < high; j++) {
-            if (arr[j] < pivot) {
-                i++;
-                swap(arr[i], arr[j]);
-            }
+    void mergeSort(vector<int>& arr, int left, int right) {
+        if (left < right) {
+            int mid = left + (right - left) / 2;
+            mergeSort(arr, left, mid);
+            mergeSort(arr, mid + 1, right);
+            merge(arr, left, mid, right);
         }
-        swap(arr[i + 1], arr[high]);
-        return i + 1;
     }
 };
