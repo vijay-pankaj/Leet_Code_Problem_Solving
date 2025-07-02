4d514021e3d10ef77a4867a160045f014cae23a7
vij@y_pankaJ <138501263+vijay-pankaj@users.noreply.github.com>
2025-07-02 23:11:32 +0530

    Time: 312 ms (5.04%) | Memory: 30.6 MB (29.77%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
@@ -1,15 +1,33 @@
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
-        for(int i=0; i<nums.size(); i++){
-            nums[i]=nums[i]*nums[i];
+        for (int i = 0; i < nums.size(); i++) {
+            nums[i] = nums[i] * nums[i];
         }
-        for(int i=0; i<nums.size(); i++){
-            for(int j=1; j<nums.size()-i; j++){
-                if(nums[j-1]>nums[j])swap(nums[j-1],nums[j]);
+        quickSort(nums, 0, nums.size() - 1);
+
+        return nums;
+    }
+
+    void quickSort(vector<int>& arr, int low, int high) {
+        if (low < high) {
+            int pi = part(arr, low, high);
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+
+    int part(vector<int>& arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                swap(arr[i], arr[j]);
             }
         }
-        return nums;
-
+        swap(arr[i + 1], arr[high]);
+        return i + 1;
     }
 };
