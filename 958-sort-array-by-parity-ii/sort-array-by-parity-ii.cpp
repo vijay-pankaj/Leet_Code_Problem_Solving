5c745c6a3adfe2b17d9401b7c38ae24fa891bb61
vij@y_pankaJ <138501263+vijay-pankaj@users.noreply.github.com>
2025-07-04 19:07:19 +0530

    Time: 0 ms (100.00%) | Memory: 26 MB (47.62%) - LeetSync

--- 958-sort-array-by-parity-ii/sort-array-by-parity-ii.cpp
+++ 958-sort-array-by-parity-ii/sort-array-by-parity-ii.cpp
@@ -5,9 +5,9 @@ public:
         int start = 0, end = 1;
 
         while (start < n && end < n) {
-            while (start < n && nums[start] % 2 == 0) start += 2;
-            while (end < n && nums[end] % 2 == 1) end += 2;
-            if (start < n && end < n) swap(nums[start], nums[end]);
+            while (start < n && nums[start] % 2 == 0){start += 2;}
+            while (end < n && nums[end] % 2 == 1){ end += 2;}
+            if (start < n && end < n){ swap(nums[start], nums[end]);}
         }
         return nums;
     }
