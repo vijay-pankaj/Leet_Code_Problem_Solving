27e472e1e43b889c7614878b73c0e61f759120df
vij@y_pankaJ <138501263+vijay-pankaj@users.noreply.github.com>
2025-07-10 18:40:28 +0530

    Time: 9 ms (30.10%) | Memory: 55.1 MB (47.09%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.cpp
+++ 41-first-missing-positive/first-missing-positive.cpp
@@ -1,29 +1,17 @@
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
-        // int missingNum=1;
-        //  sort(nums.begin(), nums.end());
-        // for(int i=0; i<nums.size(); i++){
-        //     if(nums[i]==missingNum){
-        //         missingNum++;
-        //     }
-        //     else if(nums[i]>missingNum){
-        //         break;
-        //     }
-        // }
-        //  return missingNum;
-        int n=nums.size();
-        for(int i=0; i<n; i++){
-            while(nums[i]>=1 && nums[i]<=n && nums[i]!=nums[nums[i]-1]){
-                swap(nums[i],nums[nums[i]-1]);
+        int missingNum=1;
+         sort(nums.begin(), nums.end());
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i]==missingNum){
+                missingNum++;
+            }
+            else if(nums[i]>missingNum){
+                break;
             }
         }
-         for (int i = 0; i < n; i++) {
-        if (nums[i] != i + 1) {
-            return i + 1;
-        }
-    }
-    return n+1;
+         return missingNum;
     }
    
 };
