c3aa329e1124f0c6fa44049f5d527a4977e1cbcd
vij@y_pankaJ <138501263+vijay-pankaj@users.noreply.github.com>
2025-07-31 19:44:39 +0530

    Time: 3 ms (82.99%) | Memory: 9.9 MB (54.76%) - LeetSync

--- 38-count-and-say/count-and-say.cpp
+++ 38-count-and-say/count-and-say.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     string countAndSay(int n) {
-        if(n==1)return "1";
-        string result="1";
-        for(int i=2; i<=n; i++){
-            string currstr="";
-            int count=1;
-            for(int j=1; j<result.size();j++){
-                if(result[j]==result[j-1]){
+        if (n == 1)
+            return "1";
+        string result = "1";
+        for (int i = 2; i <= n; i++) {
+            string currstr = "";
+            int count = 1;
+            for (int j = 1; j < result.size(); j++) {
+                if (result[j] == result[j - 1]) {
                     count++;
+                } else {
+                    currstr += to_string(count) + result[j - 1];
+                    count = 1;
                 }
-                else{
-                    currstr+=to_string(count)+result[j-1];
-                    count=1;
-                } 
             }
-              currstr += to_string(count) + result[result.size()-1];
+            currstr += to_string(count) + result[result.size() - 1];
             result = currstr;
         }
         return result;
