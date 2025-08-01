a200a31ce3bdccca6b341141934f16b056843f4d
vij@y_pankaJ <138501263+vijay-pankaj@users.noreply.github.com>
2025-08-01 22:54:49 +0530

    Time: 3 ms (17.67%) | Memory: 14.1 MB (16.43%) - LeetSync

--- 1510-find-lucky-integer-in-an-array/find-lucky-integer-in-an-array.cpp
+++ 1510-find-lucky-integer-in-an-array/find-lucky-integer-in-an-array.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
-
-    int countEle(vector<int>&arr,int n){
-        int count=0;
-        for(int i=0; i<arr.size(); i++){
-            if(arr[i]==n)count++;
-        }
-        return count;
-    }
+    // int countEle(vector<int>&arr,int n){
+    //     int count=0;
+    //     for(int i=0; i<arr.size(); i++){
+    //         if(arr[i]==n)count++;
+    //     }
+    //     return count;
+    // }
     int findLucky(vector<int>& arr) {
-        int maxLucky=-1;
-        for(int i=0; i<arr.size(); i++){
-            if(countEle(arr,arr[i])==arr[i]) maxLucky = max(maxLucky, arr[i]);
+        vector<int>freq(501,0);
+           for (int num : arr) {
+            freq[num]++;
         }
-        return maxLucky ;
+        for(int i=500; i>=1; i--){
+            if(freq[i]==i)return i;
+        }
+        return -1;
+        // int maxLucky=-1;
+        // for(int i=0; i<arr.size(); i++){
+        //     if(countEle(arr,arr[i])==arr[i]) maxLucky = max(maxLucky, arr[i]);
+        // }
+        // return maxLucky ;
     }
 };
 
-// vector<int>freq(501,0);
-        //    for (int num : arr) {
-        //     freq[num]++;
-        // }
-        // for(int i=500; i>=1; i--){
-        //     if(freq[i]==i)return i;
-        // }
-        // return -1;
