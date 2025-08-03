4d5c199837ac7e72aadc67a73f96318b46f5f9be
vij@y_pankaJ <138501263+vijay-pankaj@users.noreply.github.com>
2025-08-03 22:50:55 +0530

    Time: 5 ms (91.55%) | Memory: 9.5 MB (68.35%) - LeetSync

---
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -0,0 +1,65 @@
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        if (s.empty())
+            return "";
+
+        int start = 0, maxLength = 1;
+
+        for (int i = 0; i < s.size(); ++i) {
+            int l = i, r = i;
+            while (l >= 0 && r < s.size() && s[l] == s[r]) {
+                if (r - l + 1 > maxLength) {
+                    start = l;
+                    maxLength = r - l + 1;
+                }
+                l--;
+                r++;
+            }
+
+            l = i;
+            r = i + 1;
+            while (l >= 0 && r < s.size() && s[l] == s[r]) {
+                if (r - l + 1 > maxLength) {
+                    start = l;
+                    maxLength = r - l + 1;
+                }
+                l--;
+                r++;
+            }
+        }
+
+        return s.substr(start, maxLength);
+    }
+};
+
+// class Solution {
+// public:
+//     bool ispalindrom(string str) {
+//         int s = 0, e = str.size() - 1;
+//         while (s < e) {
+//             if (str[s] != str[e])
+//                 return false;
+//             s++;
+//             e--;
+//         }
+//         return true;
+//     }
+
+//     string longestPalindrome(string s) {
+//         string longeststr = "";
+
+//         for (int i = 0; i < s.size(); i++) {
+//             string currentstr = "";
+//             for (int j = i; j < s.size(); j++) {
+//                 currentstr += s[j];
+//                 if (ispalindrom(currentstr) &&
+//                     currentstr.size() > longeststr.size()) {
+//                     longeststr = currentstr;
+//                 }
+//             }
+//         }
+
+//         return longeststr;
+//     }
+// };
