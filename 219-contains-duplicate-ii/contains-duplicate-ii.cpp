a26911247642eba24d970e7698c83d8cc4864b5d
vij@y_pankaJ <138501263+vijay-pankaj@users.noreply.github.com>
2025-08-10 20:51:32 +0530

    Time: 49 ms (99.00%) | Memory: 77.4 MB (91.85%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.cpp
+++ 219-contains-duplicate-ii/contains-duplicate-ii.cpp
@@ -1,10 +1,26 @@
 class Solution {
 public:
-    bool containsNearbyDuplicate(vector<int>& nums, int k) {
-        for(int i=0; i<nums.size(); i++){
-            for(int j=i+1; j<nums.size()&& j-i<= k; j++){
-                if(nums[i]==nums[j])
+
+    bool checkDuplicates(vector<int> nums) {
+        sort(nums.begin(), nums.end());
+        for (int i = 0; i < nums.size() - 1; ++i) {
+            if (nums[i] == nums[i + 1]) {
                 return true;
+            }
+        }
+        return false;
+    }
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        
+        if (!checkDuplicates(nums)) {
+            return false;
+        }
+        for (int i = 0; i < nums.size(); ++i) {
+            for (int g = 0; g < nums.size(); ++g) {
+                int absDist = abs(i - g);
+                if ((nums[i] == nums[g]) && (absDist <= k) && (i != g)) {
+                    return true;
+                }
             }
         }
         return false;
