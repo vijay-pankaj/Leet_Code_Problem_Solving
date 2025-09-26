64105fa2c2d8ef0176753428da6e2f3d82047292
vij@y_pankaJ <138501263+vijay-pankaj@users.noreply.github.com>
2025-09-26 14:58:55 +0530

    Time: 11 ms (5.51%) | Memory: 13.8 MB (90.11%) - LeetSync

---
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
@@ -0,0 +1,24 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> arr;
+        for (int i = 0; i < nums1.size(); i++) {
+            for (int j = 0; j < nums2.size(); j++) { 
+                if (nums1[i] == nums2[j]) {
+                    bool found = false;
+                    for (int k = 0; k < arr.size(); k++) {
+                        if (arr[k] == nums1[i]) {
+                            found = true;
+                            break;
+                        }
+                    }
+                    if (!found) arr.push_back(nums1[i]); 
+                }
+            }
+        }
+        return arr;
+    }
+};
