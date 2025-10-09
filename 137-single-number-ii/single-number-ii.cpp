d31d9c197a27cf1b02a5354c109065588670b0e3
vij@y_pankaJ <138501263+vijay-pankaj@users.noreply.github.com>
2025-10-09 15:43:05 +0530

    Time: 63 ms (5.55%) | Memory: 13.4 MB (66.32%) - LeetSync

--- 137-single-number-ii/single-number-ii.cpp
+++ 137-single-number-ii/single-number-ii.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        for(int i=0; i<nums.size(); i++){
-             int count=0;
-            bool check=false;
-            for(int j=0; j<nums.size(); j++){
-                if(nums[j]==nums[i])count++;
-                if(count>1){
-                    check=true;
+        for (int i = 0; i < nums.size(); i++) {
+            int count = 0;
+            bool check = false;
+            for (int j = 0; j < nums.size(); j++) {
+                if (nums[j] == nums[i])
+                    count++;
+                if (count > 1) {
+                    check = true;
                     break;
                 }
             }
-            if(!(check)){
+            if (!(check)) {
                 return nums[i];
             }
         }
         return -1;
-
     }
 };
