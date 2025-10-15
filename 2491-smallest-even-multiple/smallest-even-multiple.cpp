92c647d13dbe925233830eab3b7b1c0f9edb69d1
vij@y_pankaJ <138501263+vijay-pankaj@users.noreply.github.com>
2025-10-15 22:47:10 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (62.09%) - LeetSync

--- 2491-smallest-even-multiple/smallest-even-multiple.cpp
+++ 2491-smallest-even-multiple/smallest-even-multiple.cpp
@@ -1,9 +1,8 @@
 class Solution {
 public:
     int smallestEvenMultiple(int n) {
-        if (n % 2 == 0)
-            return n;
-        else
-            return n * 2;
+         int smallesmul=n % 2 == 0? n:n*2;
+         return smallesmul;
+       
     }
 };
