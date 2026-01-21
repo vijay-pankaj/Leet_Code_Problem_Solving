ef54df2c778d90909442d64490e76188c52ac504
vij@y_pankaJ <138501263+vijay-pankaj@users.noreply.github.com>
2026-01-22 00:44:00 +0530

    Time: 152 ms (51.46%) | Memory: 69.4 MB (76.28%) - LeetSync

--- 164-maximum-gap/maximum-gap.js
+++ 164-maximum-gap/maximum-gap.js
@@ -2,13 +2,25 @@
  * @param {number[]} nums
  * @return {number}
  */
+// var maximumGap = function(nums) {
+    // if(nums.length<2)return 0;
+    // let maxgap=0;
+    // nums.sort((a,b)=>a-b);
+    // for(let i=0; i<nums.length; i++){
+    //     let gap=nums[i]-nums[i-1];
+    //     if(gap>maxgap)maxgap=gap;
+    // }
+    // return maxgap;
+// }
+
 var maximumGap = function(nums) {
-    if(nums.length<2)return 0;
-    let maxgap=0;
-    nums.sort((a,b)=>a-b);
-    for(let i=0; i<nums.length; i++){
-        let gap=nums[i]-nums[i-1];
-        if(gap>maxgap)maxgap=gap;
+    if (nums.length < 2) return 0;
+
+    nums.sort((a, b) => a - b);
+
+    let maxGap = 0;
+    for (let i = 1; i < nums.length; i++) {
+        maxGap = Math.max(maxGap, nums[i] - nums[i - 1]);
     }
-    return maxgap;
-}
+    return maxGap;
+};
