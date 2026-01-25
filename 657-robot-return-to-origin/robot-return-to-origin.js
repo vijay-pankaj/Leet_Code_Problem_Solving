7e0630cc452bba69d7d56660812fb63c6d64b228
vij@y_pankaJ <138501263+vijay-pankaj@users.noreply.github.com>
2026-01-26 02:33:58 +0530

    Time: 5 ms (80.47%) | Memory: 56.6 MB (49.83%) - LeetSync

---
+++ 657-robot-return-to-origin/robot-return-to-origin.js
@@ -0,0 +1,10 @@
+var judgeCircle = function(moves) {
+    let x = 0,y = 0;
+    for (let i = 0; i < moves.length; i++) {
+        if (moves[i] === 'U') y += 1;
+        if (moves[i] === 'D') y -= 1;
+        if (moves[i] === 'L') x -= 1;
+        if (moves[i] === 'R') x += 1;
+    }
+    return x === 0 && y === 0;
+};
